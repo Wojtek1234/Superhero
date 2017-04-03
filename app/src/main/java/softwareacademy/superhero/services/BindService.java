739b337bbbd65b91d92efaa49f93d552a866c7e1
wojciechmaciejewski <wojciechmaciejewski1985@ga>
2017-04-03 18:36:38 +0200

    create new Services classes

---
+++ app/src/main/java/softwareacademy/superhero/services/BindService.java
@@ -0,0 +1,18 @@
+package softwareacademy.superhero.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class BindService extends Service {
+    public BindService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+
+}
